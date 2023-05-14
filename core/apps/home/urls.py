37551d09e6b82d0bb214b3242283ac7595fce7c4
Tony Lee <leegreen123@naver.com>
2023-05-14 19:12:18 +0900

    fixed: unused imports

--- core/apps/home/urls.py
+++ core/apps/home/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-from .views import HomeView, SignUpView, LoginView, SuccessView
-from django.contrib.auth.decorators import login_required
+from .views import HomeView
 
 
 app_name = "home"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path("signup/", SignUpView.as_view(), name="signup"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("success/", login_required(SuccessView.as_view()), name="success"),
 ]
