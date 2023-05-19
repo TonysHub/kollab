a96bf8ee4af575912d7a2cd9eae2ad7b9f73f60c
yoon-js <hyper20151501@gmail.com>
2023-05-19 09:24:28 +0900

    login, signup, campaign form, model attr  updated

--- core/apps/campaign/forms.py
+++ core/apps/campaign/forms.py
@@ -13,26 +13,3 @@ class CampaignCreateForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(CampaignCreateForm,self).__init__(*args, **kwargs)
-        self.fields['name'].widget.attrs['class'] = 'field--l'
-        self.fields['name'].widget.attrs['placeholder'] = 'Enter campaign name'
-        self.fields['name'].widget.attrs['required'] = 'true'
-
-        self.fields['description'].widget.attrs['class'] = 'field--l'
-        self.fields['description'].widget.attrs['placeholder'] = 'Explanation about the campaign'
-        self.fields['description'].widget.attrs['required'] = 'true'
-
-        self.fields['start_date'].widget.attrs['class'] = 'field--l'
-        self.fields['start_date'].widget.attrs['placeholder'] = 'Select starting date'
-        self.fields['start_date'].widget.attrs['required'] = 'true'
-
-        self.fields['end_date'].widget.attrs['class'] = 'field--l'
-        self.fields['end_date'].widget.attrs['placeholder'] = 'Select ending date'
-        self.fields['end_date'].widget.attrs['required'] = 'true'
-
-        self.fields['bid'].widget.attrs['class'] = 'field--l'
-        self.fields['bid'].widget.attrs['placeholder'] = 'Enter bid amount'
-        self.fields['bid'].widget.attrs['required'] = 'true'
-
-        self.fields['category'].widget.attrs['class'] = 'field--l'
-        self.fields['category'].widget.attrs['placeholder'] = 'Select category'
-        self.fields['category'].widget.attrs['required'] = 'true'
